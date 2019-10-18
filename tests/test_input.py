130da7dd15c62bfc544e3a27cc63d97c7530f7c6
Gorbunov Pavel <pachagorbunoff@yandex.ru>
2019-10-18 15:30:11 +0300

    add test on reading from file

---
+++ tests/test_input.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+import unittest as ut
+
+from Controller import prog_input
+
+class TestInput(ut.TestCase):
+    def test_f_param(self):
+        with open('input_test.txt', 'r') as f:
+            f_string = f.readline().strip()
+            data = prog_input.get_f_parameter(f_string)
+        
+        self.assertEqual(data['f_params'][1], 30)
+
+
+    def test_input_from_file(self):
+        with open('input_test.txt', 'r') as f:
+            data = prog_input.read_from_file(f)
+        
+        print(data)
+        self.assertEqual(data['func'], 'max')
